2443f0ffad14e4adfcba4d17899cbf2d560546c8
Davi Kawasaki <davishinjik@gmail.com>
2017-05-23 15:39:12 -0300

    Until class 4.6 - Adding CORS and OPTIONS permitAll

--- src/main/java/com/algaworks/socialbooks/resources/LivrosResource.java
+++ src/main/java/com/algaworks/socialbooks/resources/LivrosResource.java
@@ -13,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +26,7 @@ import com.algaworks.socialbooks.domain.Livro;
 import com.algaworks.socialbooks.services.LivrosService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/livros")
 public class LivrosResource {
 	
